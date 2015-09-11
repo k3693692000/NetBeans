07a9916953bc821404c7fe45b718a09fc00ef1ca
Seongman Kim <k3693692000@gmail.com>
2015-09-10 19:43:29 -0600

    npm setup-ed

--- web/app.js
+++ web/app.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req,res) {
-    res.send('Hello World');
-});
+app.use(express.static(__dirname);
+
+//app.get('/', function (req,res) {
+//    res.send('Hello World');
+//});
 
 app.listen(3000, function(){
     console.log('Server On!');
